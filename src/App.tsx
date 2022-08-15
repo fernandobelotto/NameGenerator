818e9155c062c98ffdc1c8b1df9a10f1e47989a5
Fernando <fernando.bbosco@gmail.com>
2022-08-15 08:04:03 -0300

    quite done

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
-import { Center } from "@chakra-ui/react";
-import AppButton from "./components/Button";
+import { Button, Center, Heading, VStack } from "@chakra-ui/react";
+import { faker } from "@faker-js/faker";
+import { useState } from "react";
 
 export default function App() {
+  function generateName() {
+    setRandomName(faker.name.fullName());
+  }
+  const [randomName, setRandomName] = useState("");
   return (
     <Center h="100vh">
-      <AppButton>Do Something!</AppButton>
+      <VStack>
+        <Button onClick={generateName}>Generate Random Name</Button>
+        <Heading>{randomName}</Heading>
+      </VStack>
     </Center>
   );
 }
